99bd4147ce603354fa3802a0ae7a7a835df36402
matiechaza <echazarreta.matias@gmail.com>
2023-08-12 12:28:54 -0300

    Added logic for new product

--- src/CafeAltocusco.php
+++ src/CafeAltocusco.php
@@ -9,11 +9,17 @@ final class CafeAltocusco extends Product
 
     protected function updateQuality()
     {
-        
+        if ($this->quality->isGreaterThanMinValue()) {
+            $this->quality = $this->quality->decrease(2);
+        }
     }
 
     protected function updateQualityAfterSellIn()
     {
+        if (!$this->sellIn->isNegative()) {
+            return;
+        }
 
+        $this->updateQuality();
     }
 }
